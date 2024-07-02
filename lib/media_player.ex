4919430945483e3f3bc9b661e45aabeed5561e2c
Márcio Júnior <marcio.mj70@gmail.com>
2024-07-01 23:22:47 -0300

    init project

---
+++ lib/media_player.ex
@@ -0,0 +1,18 @@
+defmodule MediaPlayer do
+  @moduledoc """
+  Documentation for `MediaPlayer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MediaPlayer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule MediaPlayer.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :media_player,
+      version: "0.1.0",
+      elixir: "~> 1.17",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:dotenv, "~> 3.0"},
+      {:decimal, "~> 1.0"},
+      {:xandra, "~> 0.14"},
+      {:elixir_uuid, "~> 1.2"}
+    ]
+  end
+end
