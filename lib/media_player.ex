89bb998979fdeec248859cf11f0ca1eba8c0b94e
Márcio Júnior <marcio.mj70@gmail.com>
2024-07-02 00:17:50 -0300

    feat: user menu

--- lib/media_player.ex
+++ lib/media_player.ex
@@ -1,18 +1,55 @@
 defmodule MediaPlayer do
-  @moduledoc """
-  Documentation for `MediaPlayer`.
-  """
+  alias MediaPlayer.Commands
 
-  @doc """
-  Hello world.
+  def start(_, _) do
+    run()
+    {:ok, self()}
+  end
 
-  ## Examples
+  def run do
+    IO.puts("-------------------------------------")
+    IO.puts("- ScyllaDB Cloud Elixir Media Player -")
+    IO.puts("- Leave a star on the repo -")
+    IO.puts("-------------------------------------")
+    IO.puts("Here some possibilities")
+    IO.puts(" !add - add new song")
+    IO.puts(" !list - list all songs")
+    IO.puts(" !delete - delete a specific song")
+    IO.puts(" !stress - stress testing with mocked data")
+    IO.puts("-------------------------------------")
 
-      iex> MediaPlayer.hello()
-      :world
+    loop()
+  end
 
-  """
-  def hello do
-    :world
+  def loop do
+    IO.puts("-------------------------------------")
+    IO.puts("Type any command: ")
+    command = IO.gets("") |> String.trim()
+
+    case command do
+      "!add" ->
+        Commands.add()
+        loop()
+
+      "!list" ->
+        Commands.list()
+        loop()
+
+      "!delete" ->
+        Commands.delete()
+        loop()
+
+      "!stress" ->
+        Commands.stress()
+        loop()
+
+      "exit" ->
+        IO.puts("Bye bye!")
+        :OK
+
+      _ ->
+        IO.puts("Command not found!")
+        loop()
+    end
   end
 end
--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule MediaPlayer.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {MediaPlayer, []},
       extra_applications: [:logger]
     ]
   end
